f31c69204c75b295d2bf543ee79d1f803f61a189
junnyJun <chbe5082@naver.com>
2023-04-18 16:12:35 +0900

    config : out api

---
+++ src/main/kotlin/tv/junu/video/video/gateway/out/UploadVideo.kt
@@ -0,0 +1,11 @@
+package tv.junu.video.video.gateway.out
+
+interface UploadVideo {
+    fun upload()
+
+    class UploadMinIoGateway(
+
+    ){
+
+    }
+}
---
+++ src/main/kotlin/tv/junu/video/video/gateway/out/VideoPart.kt
@@ -0,0 +1,8 @@
+package tv.junu.video.video.gateway.out
+
+interface VideoPart {
+
+    class VideoMinIoGateway{
+
+    }
+}
