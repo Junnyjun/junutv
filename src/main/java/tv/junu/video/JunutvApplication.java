81a0b349c193edb3f8de20e32afac81035427014
junnyJun <chbe5082@naver.com>
2023-04-18 15:41:40 +0900

    init

---
+++ src/main/java/tv/junu/video/JunutvApplication.java
@@ -0,0 +1,13 @@
+package tv.junu.video;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunutvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunutvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tv/junu/video/JunutvApplicationTests.java
@@ -0,0 +1,13 @@
+package tv.junu.video;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunutvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
