29a5b6df74d0e08ac05ef0f84b0ee630e4bd6237
junnyJun <chbe5082@naver.com>
2023-04-19 12:49:10 +0900

    feat : get video part

--- src/main/kotlin/tv/junu/video/video/gateway/out/VideoPart.kt
+++ src/main/kotlin/tv/junu/video/video/gateway/out/VideoPart.kt
@@ -1,8 +1,23 @@
 package tv.junu.video.video.gateway.out
 
-interface VideoPart {
+import io.minio.MinioClient
+import org.springframework.stereotype.Component
 
-    class VideoMinIoGateway{
+interface VideoPart {
+    fun get(videoPart: GetVideoRequest): ByteArray
+
+    @Component
+    class VideoMinIoGateway(
+        private val minioClient: MinioClient,
+    ):VideoPart{
+        override fun get(videoPart: GetVideoRequest): ByteArray {
+            TODO("Not yet implemented")
+        }
 
     }
+
+    data class GetVideoRequest(
+        val videoName: String,
+        val startRange:Long,
+    )
 }
