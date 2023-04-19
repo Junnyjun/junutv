e794c70a0c94354c4c4ad328ab102d294eef478b
junnyJun <chbe5082@naver.com>
2023-04-19 13:23:32 +0900

    feat : get part

--- src/main/kotlin/tv/junu/video/video/gateway/out/file/VideoPart.kt
+++ src/main/kotlin/tv/junu/video/video/gateway/out/file/VideoPart.kt
@@ -1,27 +1,34 @@
 package tv.junu.video.video.gateway.out.file
 
+import io.minio.GetObjectArgs
 import io.minio.MinioClient
 import org.springframework.stereotype.Component
+import java.io.InputStream
 
 
 interface VideoPart {
-    fun get(videoPart: GetVideoRequest): ByteArray
+    fun get(videoPart: GetVideoRequest): InputStream
 
-    private val chunkSize: Long get() = 1024 * 1024
+    val chunkSize: Long get() = 1024 * 1024
 
     @Component
     class VideoMinIoGateway(
         private val minioClient: MinioClient,
-    ): VideoPart {
+    ) : VideoPart {
 
-        override fun get(videoPart: GetVideoRequest): ByteArray {
-            TODO("Not yet implemented")
-        }
-
+        override fun get(videoPart: GetVideoRequest): InputStream = minioClient.getObject(
+            GetObjectArgs.builder()
+                .bucket("junu-tv")
+                .`object`(videoPart.videoName)
+                .offset(videoPart.startRange)
+                .length(chunkSize)
+                .build()
+        )
     }
 
     data class GetVideoRequest(
         val videoName: String,
-        val startRange:Long,
+        val videoLength: Long,
+        val startRange: Long,
     )
 }
