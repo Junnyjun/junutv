443b7b7f6855f6000bcdf2cfc847dc409d87ab0f
junnyJun <chbe5082@naver.com>
2023-04-19 12:48:36 +0900

    feat : upload video

--- src/main/kotlin/tv/junu/video/video/gateway/out/UploadVideo.kt
+++ src/main/kotlin/tv/junu/video/video/gateway/out/UploadVideo.kt
@@ -1,11 +1,33 @@
 package tv.junu.video.video.gateway.out
 
+import io.minio.MinioClient
+import io.minio.ObjectWriteResponse
+import io.minio.PutObjectArgs
+import org.springframework.stereotype.Component
+import java.io.File
+
 interface UploadVideo {
-    fun upload()
+    fun upload(video: VideoRequest): ObjectWriteResponse
 
+    @Component
     class UploadMinIoGateway(
-
-    ){
-
+        private val minioClient: MinioClient,
+    ) : UploadVideo {
+        override fun upload(video: VideoRequest) = minioClient.putObject(
+            PutObjectArgs.builder()
+                .bucket("junu-tv")
+                .`object`(video.name)
+                .contentType("video/mp4")
+                .tags(mapOf("category" to video.tag))
+                .stream(video.file.inputStream(), video.file.length(), -1)
+                .build()
+        )
     }
+
+    data class VideoRequest(
+        val name: String,
+        val contentType: String,
+        val tag: String,
+        val file: File,
+    )
 }
---
+++ src/test/java/tv/junu/video/video/gateway/out/UploadVideoTest.kt
@@ -0,0 +1,21 @@
+package tv.junu.video.video.gateway.out
+
+import org.junit.jupiter.api.Test
+import tv.junu.video.config.MinioConfig
+import java.nio.file.Path
+
+class UploadVideoTest{
+    val gateway = UploadVideo.UploadMinIoGateway(MinioConfig().dev())
+    @Test
+    fun `upload video`(){
+        val upload = gateway.upload(
+            UploadVideo.VideoRequest(
+                "test.mp4",
+                "video/mp4",
+                "test",
+                Path.of("C:\\workspace\\junutv\\src\\main\\resources\\sample\\sample.mp4").toFile()
+            )
+        )
+        println(upload.etag())
+    }
+}
