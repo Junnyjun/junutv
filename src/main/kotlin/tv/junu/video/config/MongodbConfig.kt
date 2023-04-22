f7ae3c202f3f59ec19bc17105bdcb31a802c2aad
Mang-Joo <wjwan0915@gmail.com>
2023-04-22 22:29:14 +0900

    :sparkles: feat: update reactive library

--- src/main/kotlin/tv/junu/video/config/MongodbConfig.kt
+++ src/main/kotlin/tv/junu/video/config/MongodbConfig.kt
@@ -1,29 +1,30 @@
 package tv.junu.video.config
 
-import com.mongodb.client.MongoClient
+import com.mongodb.reactivestreams.client.MongoClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.mongodb.MongoDatabaseFactory
-import org.springframework.data.mongodb.MongoTransactionManager
+import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory
+import org.springframework.data.mongodb.ReactiveMongoTransactionManager
 import org.springframework.data.mongodb.config.EnableMongoAuditing
-import org.springframework.data.mongodb.core.MongoTemplate
-import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.data.mongodb.core.SimpleReactiveMongoDatabaseFactory
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 
 @Configuration
-@EnableMongoRepositories(basePackages = ["tv.junu.video"],)
+@EnableMongoRepositories(basePackages = ["tv.junu.video"])
 @EnableMongoAuditing
 class MongodbConfig {
 
     @Bean
-    fun transactionManager(dbFactory: MongoDatabaseFactory): MongoTransactionManager =
-        MongoTransactionManager(dbFactory)
+    fun transactionManager(dbFactory: ReactiveMongoDatabaseFactory): ReactiveMongoTransactionManager =
+            ReactiveMongoTransactionManager(dbFactory)
 
 
     @Bean
-    fun mongoTemplate(mongoClient: MongoClient): MongoTemplate {
-        val factory: MongoDatabaseFactory = SimpleMongoClientDatabaseFactory(mongoClient, "junutv")
-        return MongoTemplate(factory)
-    }
+    fun reactiveMongoTemplate(mongoClient: MongoClient): ReactiveMongoTemplate =
+            ReactiveMongoTemplate(SimpleReactiveMongoDatabaseFactory(mongoClient, "junutv"))
+//        val factory: MongoDatabaseFactory = SimpleMongoClientDatabaseFactory(mongoClient, "junutv")
+//        return MongoTemplate(factory)
+
 }
