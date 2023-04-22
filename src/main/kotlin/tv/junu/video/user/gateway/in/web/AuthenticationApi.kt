8ced9c73c4da085108c8c67d4a66c23b01a91dc9
JunnyJun <chbe5082@naver.com>
2023-04-22 23:12:18 +0900

    feat : more functional

--- src/main/kotlin/tv/junu/video/user/gateway/in/web/AuthenticationApi.kt
+++ src/main/kotlin/tv/junu/video/user/gateway/in/web/AuthenticationApi.kt
@@ -14,7 +14,7 @@ import tv.junu.video.user.exception.PasswordNotMatchException
 import tv.junu.video.user.gateway.out.repository.UserRepository
 import tv.junu.video.user.gateway.out.repository.jpa.UserEntity
 
-sealed interface AuthenticationApi{
+sealed interface AuthenticationApi {
     fun login(authRequest: AuthRequest): Mono<ResponseEntity<AuthResponse>>
     fun signup(authRequest: AuthRequest): Mono<ResponseEntity<AuthResponse>>
     fun signin(authRequest: AuthRequest): Mono<ResponseEntity<AuthResponse>>
@@ -25,25 +25,29 @@ sealed interface AuthenticationApi{
         private val passwordEncoder: PasswordEncoder,
         private val findUser: FindUser,
         private val userRepository: UserRepository
-    ):AuthenticationApi {
+    ) : AuthenticationApi {
         @PostMapping("/api/v1/login")
         override fun login(@RequestBody authRequest: AuthRequest): Mono<ResponseEntity<AuthResponse>> =
             findUser.ByEmail(authRequest.email)
                 .filter { passwordEncoder.matches(authRequest.password, it.password) }
-                .map { ResponseEntity.ok(AuthResponse(tokenCreate.createToken(it.id, it.role))) }
+                .map { tokenCreate.createToken(TokenCreateRequest(it.id, it.role)) }
+                .map { ResponseEntity.ok(AuthResponse(it)) }
                 .switchIfEmpty(Mono.error(PasswordNotMatchException()))
 
         @PostMapping("/api/v1/signup")
         override fun signup(@RequestBody authRequest: AuthRequest): Mono<ResponseEntity<AuthResponse>> =
             findUser.ByEmail(authRequest.email)
                 .filter { passwordEncoder.matches(authRequest.password, it.password) }
-                .map { ResponseEntity.ok(AuthResponse(tokenCreate.createToken(it.id, it.role))) }
+                .map { tokenCreate.createToken(TokenCreateRequest(it.id, it.role)) }
+                .map { ResponseEntity.ok(AuthResponse(it)) }
+                .switchIfEmpty(Mono.error(PasswordNotMatchException()))
 
         @PostMapping("/api/v1/regist")
         override fun signin(@RequestBody authRequest: AuthRequest): Mono<ResponseEntity<AuthResponse>> = userRepository
             .save(authRequest.toEntity)
-            .map {tokenCreate.createToken(TokenCreateRequest(it.id, it.role))}
+            .map { tokenCreate.createToken(TokenCreateRequest(it.id, it.role)) }
             .map { ResponseEntity.ok(AuthResponse(it)) }
+            .switchIfEmpty(Mono.error(PasswordNotMatchException()))
 
     }
 
@@ -55,11 +59,12 @@ sealed interface AuthenticationApi{
         val name: String,
         val email: String,
         val password: String
-    ){
-        val toEntity get() = UserEntity(
-            name = name,
-            email = email,
-            password = password
-        )
+    ) {
+        val toEntity
+            get() = UserEntity(
+                name = name,
+                email = email,
+                password = password
+            )
     }
 }
