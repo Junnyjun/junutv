a908164ad7008e6dc3e0dadb8f9eac8fe83ca166
Mang-Joo <wjwan0915@gmail.com>
2023-04-22 22:29:53 +0900

    :sparkles: feat: Security config

---
+++ src/main/kotlin/tv/junu/video/user/application/config/SecurityConfig.kt
@@ -0,0 +1,28 @@
+package tv.junu.video.user.application.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+@Configuration
+@EnableWebFluxSecurity
+@EnableReactiveMethodSecurity
+class SecurityConfig {
+    @Bean
+    fun securitygWebFilterChain(
+            httpSecurity: ServerHttpSecurity,
+    ): SecurityWebFilterChain = httpSecurity
+            .formLogin().disable()
+            .logout().disable()
+            .oauth2ResourceServer { it.jwt() }
+            .authorizeExchange()
+            .pathMatchers("/api/v1/login", "/api/v1/regist").permitAll()
+            .anyExchange().authenticated()
+            .and()
+            .csrf { it.disable() }
+            .build()
+            .also { println("securitygWebFilterChain: $it") }
+}
