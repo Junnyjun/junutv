af1738ed7185b6eadcf25d3ab3b19c0e7708f506
JunnyJun <chbe5082@naver.com>
2023-04-22 23:09:18 +0900

    feat : domain add

--- src/main/kotlin/tv/junu/video/user/gateway/in/web/AuthenticationApi.kt
+++ src/main/kotlin/tv/junu/video/user/gateway/in/web/AuthenticationApi.kt
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
+import tv.junu.video.user.FindUser
 import tv.junu.video.user.TokenCreate
 import tv.junu.video.user.TokenCreate.TokenCreateRequest
-import tv.junu.video.user.UserDetailsService
 import tv.junu.video.user.domain.Role.*
 import tv.junu.video.user.exception.PasswordNotMatchException
 import tv.junu.video.user.gateway.out.repository.UserRepository
@@ -18,19 +18,19 @@ import tv.junu.video.user.gateway.out.repository.jpa.UserEntity
 class AuthenticationApi(
     private val tokenCreate: TokenCreate,
     private val passwordEncoder: PasswordEncoder,
-    private val userDetailsService: UserDetailsService,
+    private val findUser: FindUser,
     private val userRepository: UserRepository
 ) {
     @PostMapping("/api/v1/login")
     fun customLogin(@RequestBody authRequest: AuthRequest): Mono<ResponseEntity<AuthResponse>> =
-        userDetailsService.loadUserByUsername(authRequest.email)
+        findUser.ByEmail(authRequest.email)
             .filter { passwordEncoder.matches(authRequest.password, it.password) }
             .map { ResponseEntity.ok(AuthResponse(tokenCreate.createToken(it.id, it.role))) }
             .switchIfEmpty(Mono.error(PasswordNotMatchException()))
 
     @PostMapping("/api/v1/signup")
     fun customSignup(@RequestBody authRequest: AuthRequest): Mono<ResponseEntity<AuthResponse>> =
-        userDetailsService.loadUserByUsername(authRequest.email)
+        findUser.ByEmail(authRequest.email)
             .filter { passwordEncoder.matches(authRequest.password, it.password) }
             .map { ResponseEntity.ok(AuthResponse(tokenCreate.createToken(it.id, it.role))) }
 
